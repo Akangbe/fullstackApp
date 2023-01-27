e9c3a9741b28d04e6788b819097cce3c87e680e8
akangbe <akangbehenry28@gmail.com>
2023-01-27 17:22:57 +0100

    add

--- script.js
+++ script.js
@@ -13,11 +13,3 @@ btn.addEventListener("click", function () {
     btn.innerHTML = "Share a fact";
   }
 });
-btn.addEventListener.add{
-  h1.style(color:<ul class="list-group">
-    <li class="list-group-item active">Active item</li>
-    <li class="list-group-item">Item</li>
-    <li class="list-group-item disabled">Disabled item</li>
-  </ul>)
-  
-}
