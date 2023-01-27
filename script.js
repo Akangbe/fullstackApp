7e7af882942b39efe735882d89e0b472e74dea5a
akangbe <akangbehenry28@gmail.com>
2023-01-27 17:22:31 +0100

    add

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  //   console.log("clickeeeee");
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    // btn.textContent = "Close";
+    btn.innerHTML = "Close";
+  } else {
+    form.classList.add("hidden");
+    // btn.textContent = "Share a fact";
+    btn.innerHTML = "Share a fact";
+  }
+});
+btn.addEventListener.add{
+  h1.style(color:<ul class="list-group">
+    <li class="list-group-item active">Active item</li>
+    <li class="list-group-item">Item</li>
+    <li class="list-group-item disabled">Disabled item</li>
+  </ul>)
+  
+}
